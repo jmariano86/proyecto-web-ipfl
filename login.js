6fa054cf69b7fcd6445de6446472c75b2be51fa3
unknown <pickobogado1887@hotmail.com>
2022-11-14 18:50:27 -0300

    subo fix en loginjs y en loginhtml para que redirija bien

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ function validate(){
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     if (username ="maria" && password =="111") {
-        window.location.href="home.html"; 
+        window.location.href = "home.html"; 
         return false;
      } else{
         alert("usuario o password incorrecto");
@@ -13,5 +13,5 @@ let button = document.getElementById("button");
 
 button.addEventListener("click", (e)=> {
     e.preventDefault();
-    validate();
+    //validate();
 })
