079337e8e2c8d7e4e3b934422f1c035c8d17d20b
pickobogado <pickobogado1887@hotmail.com>
2022-11-22 22:05:16 -0300

    subo fix en homehtml homejs menucss menuhtml menujs stylecss

--- home.js
+++ home.js
@@ -1,17 +1,3 @@
-function validate(){
-    let 'Comprar Menu' = document.getElementById("Comprar Menu").value;
-    
-    if ( 'Comprar Menu') {
-        window.location.href="menu.html"; 
-        return false;
-     } else{
-        alert("su pedido no fue procesado");
-     }
+function redireccion(){
+    location.href="menu.html"
 }
-
-let button = document.getElementById("button");
-
-button.addEventListener("click", (e)=> {
-    e.preventDefault();
-    validate();
-})
