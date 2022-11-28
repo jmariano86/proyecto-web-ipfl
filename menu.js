bfba29a57de84b76b457481bbf958b60c30429fd
pickobogado <pickobogado1887@hotmail.com>
2022-11-28 00:13:00 -0300

    agrego id al menuhtml y menujs

--- menu.js
+++ menu.js
@@ -1,6 +1,10 @@
 function redireccion(){
     location.href="home.html"
 }
-function pagado(){
+//funcion que captura datos desde un cuadro de texto
+function pagado() {
+    var username=document.getElementById("username").value;
+    var direccion=document.getElementById("Direccion").value;
     alert ("pago");
+    console.log (username +" "+ direccion);
 }
